b5581de7454e6cde970be00eca1fb8a0d3ca8eb8
Bharath Kumar L <142790392+BharathKumar-L@users.noreply.github.com>
2024-10-28 19:43:01 +0530

    all set to show

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -62,15 +62,7 @@ import { BiLike } from "react-icons/bi";
             return (
               <div className="bg-white rounded-xl relative" key={event._id}>
               <div className='rounded-tl-[0.75rem] rounded-tr-[0.75rem] rounded-br-[0] rounded-bl-[0] object-fill aspect-16:9'>
-              {event.image && (
-                <img
-                  src={`http://localhost:4000/api/${event.image}`}
-                  alt={event.title}
-                  width="300" 
-                  height="200" 
-                  className="w-full h-full"
-                />
-              )}
+              
                 <div className="absolute flex gap-4 bottom-[240px] right-8 md:bottom-[20px] md:right-3 lg:bottom-[250px] lg:right-4 sm:bottom-[260px] sm:right-3">
                 <button onClick={() => handleLike(event._id)}>
                   <BiLike className="w-auto h-12 lg:h-10 sm:h-12 md:h-10 bg-white p-2 rounded-full shadow-md transition-all hover:text-primary" />
@@ -81,7 +73,7 @@ import { BiLike } from "react-icons/bi";
 
                 
 
-                <img src="../src/assets/paduru.png" alt="" className='rounded-tl-[0.75rem] rounded-tr-[0.75rem] rounded-br-[0] rounded-bl-[0] object-fill aspect-16:9'/> 
+                <img src="../src/assets/hackathon.png" alt="" className='rounded-tl-[0.75rem] rounded-tr-[0.75rem] rounded-br-[0] rounded-bl-[0] object-fill aspect-16:9'/> 
     {/* FIXME: This is a demo image after completing the create event function delete this */}
 
               <div className="m-2 grid gap-2">
--- client/src/pages/PaymentSummary.jsx
+++ client/src/pages/PaymentSummary.jsx
@@ -117,6 +117,7 @@ export default function PaymentSummary() {
     setRedirect(true)
     console.log('Success creating ticket', updatedTicketDetails)
   } catch (error) {
+    alert("Signin to make payment");
     console.error('Error creating ticket:', error);
   }
 
